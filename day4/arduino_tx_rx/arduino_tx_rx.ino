99a30e437e184eaa2c278761d912d41524500a4b
pierdr <pierdr@yahoo.it>
2016-06-23 15:10:34 +0200

    day4 examples

--- day4/arduino_tx_rx/arduino_tx_rx.ino
+++ day4/arduino_tx_rx/arduino_tx_rx.ino
@@ -1,50 +1,20 @@
-/*
-  Serial Event example
-
- When new serial data arrives, this sketch adds it to a String.
- When a newline is received, the loop prints the string and
- clears it.
-
- A good test for this is to try it with a GPS receiver
- that sends out NMEA 0183 sentences.
-
- Created 9 May 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/SerialEvent
-
- */
-unsigned long test;
+unsigned long testEvent;
 
 void setup() {
-  // initialize serial:
+ 
   Serial.begin(115200);
  
-  test=0;
+  testEvent=0;
 }
 
 void loop() {
-  if(millis()-test>5000)
+  if(millis()-testEvent>5000)
   {
-    test=millis();
+    testEvent=millis();
     Serial.write("test");
   }
-  serialEvent();
+ 
 }
 
-/*
-  SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
- time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
- */
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    Serial.println(Serial.readString());
-  }
-}
 
 
